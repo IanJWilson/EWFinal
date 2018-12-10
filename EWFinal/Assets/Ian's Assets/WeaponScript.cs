d627f6c3e92dff6019fb8141e3b7ba9de7b3fb76
IanJWilson <ianjwilson8@outlook.com>
2018-12-09 23:52:03 -0500

    first push

---
+++ EWFinal/Assets/Ian's Assets/WeaponScript.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    public bool IsAttacking;
+    public bool HitShield;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            IsAttacking = true;
+            //Do damage
+           // other.GetComponent<Enemy>().EnemyHealth--;
+            IsAttacking = false;
+        }
+        if (other.CompareTag("Player"))
+        {
+            IsAttacking = true;
+            //Do damage
+           // other.GetComponent<Player>().PlayerHealth--;
+            IsAttacking = false;
+        }
+    }
+
+
+}
