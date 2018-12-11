03003d39f21b7bb867c1f485040584b396ef8ab8
IanJWilson <ianjwilson8@outlook.com>
2018-12-11 09:37:49 -0500

    spear weapon and separate scenes

---
+++ EWFinal/Assets/Ian's Assets/PLayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayer : MonoBehaviour {
+
+    public float PlayerHealth;
+
+	// Use this for initialization
+	void Start () {
+        PlayerHealth = 10;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- EWFinal/Assets/Ian's Assets/WeaponScript.cs
+++ EWFinal/Assets/Ian's Assets/WeaponScript.cs
@@ -7,36 +7,67 @@ public class WeaponScript : MonoBehaviour
     public bool IsAttacking;
     public bool HitShield;
     public bool testbool = true;
+    Vector3 pointA;
+    Vector3 pointB;
+
+
+    void Start()
+    {
+        pointA = new Vector3(0.6f, 0, 0.5f);
+        pointB = new Vector3(0.6f, 0, 1.5f);
+    }
 
     void Update()
-    {
-        if (testbool == true)
+    {           // In future I will also be checking here what weapon is active to change the type of attack if necessary
+        if (Input.GetKeyDown("2"))
         {
-            Vector3 WeaponSize = GetComponent<MeshRenderer>().bounds.size;
-            Debug.Log(WeaponSize.z);
-            transform.position = Vector3.Lerp(gameObject.transform.position, gameObject.transform.position + gameObject.transform.position, 1.5f);
-            testbool = false;
 
+            float time = Mathf.PingPong(Time.time * 1f, 1);
+            transform.localPosition = Vector3.Lerp(pointA, pointB, time);
         }
+
+  
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy"))
+        //if (other.CompareTag("Enemy"))
+        //{
+        //    IsAttacking = true;
+        //    //Do damage
+        //  //  other.GetComponent<EnemyWheel>().EnemyHealth--;
+        //    IsAttacking = false;
+        //}
+        if (other.CompareTag("Player"))
         {
             IsAttacking = true;
             //Do damage
-            other.GetComponent<EnemyWheel>().EnemyHealth--;
-            IsAttacking = false;
-        }
-        else if (other.CompareTag("Player"))
-        {
-            IsAttacking = true;
-            //Do damage
-            other.GetComponent<PlayerWheel>().PlayerHealth--;
-            IsAttacking = false;
+           other.GetComponent<PLayer>().PlayerHealth--;
+            
         }
     }
+
+
+
+
+
+    //if (Input.GetKeyDown("1"))
+    //{
+    //    transform.position += Vector3.forward * Mathf.PingPong(Time.time * 0.1f, 10f);
+    //}
+
+    //if (testbool == true)
+    //{
+    //    Vector3 WeaponSize = GetComponent<MeshRenderer>().bounds.size;
+    //    Debug.Log(WeaponSize.z);
+    //    transform.position = Vector3.Lerp(gameObject.transform.position, gameObject.transform.position + gameObject.transform.position, 1.5f);
+    //    testbool = false;
+
+    //}
+
+
+
+
 
     /*public void Thrust(GameObject weapon)
     {
@@ -47,5 +78,8 @@ public class WeaponScript : MonoBehaviour
         Target.velocity = (Target.transform.forward * -3f);
     
     }*/
+
+
+
 
 }
