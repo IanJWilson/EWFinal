da9ffcaef6c9aca7ff4c1793c46714a39178f780
IanJWilson <ianjwilson8@outlook.com>
2018-12-16 20:23:41 -0500

    menu stuff

---
+++ EWFinal/Assets/Ian's Assets/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance;
+
+    public bool IsDead;
+
+
+
+    private void MakeSingleton()
+    {
+        if(instance == null)
+        {
+            instance = new GameManager();
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+
+
+
+
+
+}
